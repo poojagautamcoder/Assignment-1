dc4639fe3147a353b8560c480636b3f50e06ae03
poojagautamcoder <arun.etech11@gmail.com>
2021-12-15 15:05:27 +0530

    Code Formatting

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Layout from './Components/Project/Layout';
 import Navbar from './Components/Navbar/Navbar';
 import Heading from './Components/Heading';
 import Footer from './Footer/Footer';
-function App() {
+const App = () => {
   const data = [0,1,2,3,4,5,6,7,8]
   return (
     <div className="App">
@@ -14,7 +14,12 @@ function App() {
       <div className="card-parent">
       <div className="Cards">
       {data.map((cards,index) =>{
-        return <Layout key={index}/>
+        return (
+
+          <div key={index}>
+            <Layout/>
+          </div>
+        )
 
       })}
       </div>
